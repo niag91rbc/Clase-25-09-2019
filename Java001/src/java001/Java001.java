e41a170a21718fd6ce05e50516ab6cd703a27e90
fotocopia <catalina.pimentel@uniacc.edu>
2019-09-25 17:32:21 -0300

    actualizacion master

---
+++ Java001/src/java001/Java001.java
@@ -0,0 +1,35 @@
+
+package java001;
+
+public class Java001 {
+
+    public static void main(String[] args) {
+        // variables: son espacio de memoria
+        int a;
+        float b;
+        
+        // operadoradores aritméticos
+        // (+) adición o suma
+        // (-) sustracción o resta
+        // (*) producto o multiplicación
+        // (/) couciente o división
+        
+        int r1;
+        float r2;
+        
+        a=13;
+        b=5.2f;
+        
+        // fin de la entrada de datos
+        
+        // proceso obtener el producto de tipo int, float        
+        r1=(int)(a*b); 
+        r2=a*b;
+        
+        // Resultados o salida
+        System.out.println("Resultado 1 : "+r1);
+        System.out.println("Resultado 2 : "+r2);
+        
+    }
+    
+}
